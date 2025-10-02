4339f8ba45e7ee55dc4f8c215275fc28d483859a
M1-MQ1-D <M1-MQ1-D@DESKTOP-5FMH86R>
2025-10-02 07:44:51 -0700

    primer commit

---
+++ Servidorsote/src/Servidorsote.java
@@ -0,0 +1,25 @@
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.HashMap;
+
+public class Servidorsote{
+    static HashMap<String,UnCliente> clientes = new HashMap<String,UnCliente>();
+    
+    public static void main (String[] args) throws IOException{
+    ServerSocket servidorSocker = new ServerSocket (8080);
+    int contador = 0;
+        while (true) {            
+            Socket s = servidorSocker.accept();
+            UnCliente unCliente = new UnCliente(s);
+            Thread hilo = new Thread(unCliente);
+            clientes.put(Integer.toString(contador),unCliente);
+            hilo.start();
+            System.out.println("Se conecto el wey n: "+contador+1);
+            contador++;
+        }
+    }
+    
+}
+ 
