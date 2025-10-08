474a20093c65856e7cb4319a08c702933f39fd09
M1-MQ1-D <M1-MQ1-D@DESKTOP-5FMH86R>
2025-10-08 07:51:47 -0700

    hice el metodo obtener destino

--- Servidorsote/src/Mensaje.java
+++ Servidorsote/src/Mensaje.java
@@ -1,13 +1,20 @@
 public class Mensaje {
     String mensaje;
+    Tipo tipo;
     public enum Tipo{
-        unicast,
-        multicast,
-        broadcast
+        uni,
+        multi,
+        broad
     }
     
-    public Mensaje(String mensaje) {
+    public Mensaje(Tipo tipo, String mensaje) {
+        obtenerDestinos(tipo, mensaje);
         //this.mensaje = mensaje;
     }
     
+    public static String obtenerDestinos(Tipo tipo, String mensaje){
+        
+        
+        return "";
+    }
 }
