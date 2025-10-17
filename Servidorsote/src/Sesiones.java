8d2705c0bbf1446425a8ca4ee8284d66127fe5cf
MangoSalazar <l22440193@mochis.tecnm.mx>
2025-10-17 16:30:04 -0700

    Añadi la clase de sesiones

---
+++ Servidorsote/src/Sesiones.java
@@ -0,0 +1,19 @@
+public class Sesiones {
+    String nombre;
+    String contraseña;
+
+    public Sesiones(String nombre, String contraseña) {
+        if (numCaracteres(nombre , contraseña )) {
+            this.nombre = nombre;
+            this.contraseña = contraseña;
+        }
+    }
+    
+    private boolean numCaracteres (String nombre, String contraseña){
+        if (nombre.length() < 15 && nombre.length() > 3  && contraseña.length() < 10 && contraseña.length() > 3 ) {
+            return true;
+        }
+        return false;
+    }
+
+}
