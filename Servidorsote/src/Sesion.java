57295ebba8ae063e2ac5ca77b183a6085d89cf53
MangoSalazar <l22440193@mochis.tecnm.mx>
2025-11-18 18:52:41 -0700

    Hice el constructor de la clase sesion

--- Servidorsote/src/Sesion.java
+++ Servidorsote/src/Sesion.java
@@ -2,8 +2,34 @@
 
 
 public class Sesion {
-
+    private final UsuarioDAO usuarioDAO = new UsuarioDAO();
     String nombre;
     String contraseña;
+    public Sesion(String tipoDeInicio, String nombre, String contraseña) throws Exception {
+            if (!esValido(nombre, contraseña)) {
+                throw new Exception("Nombre o contraseña no cumplen con la longitud requerida.");
+            }
+            this.nombre = nombre;
+            this.contraseña = contraseña;
+            procesarSolicitud(tipoDeInicio);
+    }
+    private boolean esValido(String nombre, String contraseña) {
+        boolean nombreValido = nombre.length() < Protocolo.MAX_LONG_USER && nombre.length() > Protocolo.MIN_LONG_USER;
+        boolean passValido = contraseña.length() < Protocolo.MAX_LONG_PASS && contraseña.length() > Protocolo.MIN_LONG_PASS;
+        return nombreValido && passValido;
+    }
 
+    private void procesarSolicitud(String tipoDeInicio) throws Exception {
+        if (tipoDeInicio.equals(Protocolo.CMD_LOGIN)) {
+            if (!usuarioDAO.validarCredenciales(nombre, contraseña)) {
+                throw new Exception("Usuario o contraseña incorrectos");
+            }
+        } else if (tipoDeInicio.equals(Protocolo.CMD_REGISTRAR)) { // Usando constante
+            if (!usuarioDAO.registrarUsuario(nombre, contraseña)) {
+                throw new Exception("El usuario ya existe o error en base de datos");
+            }
+        } else {
+            throw new Exception("Comando de sesión desconocido");
+        }
+    }
 }
