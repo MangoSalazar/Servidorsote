3e534f5473a80c74e355ed936b0934ba04bd3981
MangoSalazar <l22440193@mochis.tecnm.mx>
2025-11-19 01:44:31 -0700

    metodo de realizarJugada añadido

--- Servidorsote/src/PartidaGato.java
+++ Servidorsote/src/PartidaGato.java
@@ -10,4 +10,17 @@ public class PartidaGato {
         this.idJugadorO = idO;
         this.turnoActual = idX;
     }
+    public String realizarJugada(int idJugador, int casilla) throws Exception {
+        if (idJugador != turnoActual) throw new Exception("No es tu turno.");
+        if (casilla < 1 || casilla > 9) throw new Exception("Casilla inválida (1-9).");
+        
+        int fila = (casilla - 1) / 3;
+        int col = (casilla - 1) % 3;
+        
+        if (tablero[fila][col] == 'X' || tablero[fila][col] == 'O') throw new Exception("Casilla ocupada.");
+        
+        tablero[fila][col] = (idJugador == idJugadorX) ? 'X' : 'O';
+        cambiarTurno();
+        return dibujarTablero();
+    }
 }
