35a2e04cadc770914bd3caa6da8eb92653d8d77d
MangoSalazar <l22440193@mochis.tecnm.mx>
2025-11-19 02:52:47 -0700

    main del servidorsote refactorizado

--- Servidorsote/src/Servidorsote.java
+++ Servidorsote/src/Servidorsote.java
@@ -8,18 +8,28 @@ public class Servidorsote{
     static HashMap<String,UnCliente> clientes = new HashMap<String,UnCliente>();
     
     public static void main (String[] args) throws IOException{
+        System.out.println("--- Iniciando Servidor ---");
         ConexionBD.inicializarBD();
-        ServerSocket servidorSocker = new ServerSocket (8080);
-        int idCliente = 0;
+        try (ServerSocket servidorSocket = new ServerSocket(Protocolo.PUERTO)) {
+            System.out.println("Servidor abiertote en el puerto: " + Protocolo.PUERTO); 
+            int idCliente = 0;
             while (true) {            
-                Socket s = servidorSocker.accept();
-                UnCliente unCliente = new UnCliente(s, Integer.toString(idCliente));
-                Thread hilo = new Thread(unCliente);
-                clientes.put(Integer.toString(idCliente),unCliente);
-                hilo.start();
-                System.out.println("Se conecto el wey n: "+idCliente);
-                idCliente++;
+                try {
+                    Socket s = servidorSocket.accept();
+                    s.setKeepAlive(true); 
+                    UnCliente unCliente = new UnCliente(s, Integer.toString(idCliente));
+                    Thread hilo = new Thread(unCliente);
+                    clientes.put(Integer.toString(idCliente), unCliente);
+                    hilo.start();
+                    System.out.println("Nueva people conectandose. ID: " + idCliente);
+                    idCliente++;
+                } catch (IOException e) {
+                    System.out.println("Error al aceptar una conexión");
+                }
             }
+        } catch (IOException e) {
+            System.out.println("Error crítico del servidor, verifica los puertos");
+        }
     }
     
 }
