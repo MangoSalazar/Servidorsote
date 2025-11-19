64ab75377da3f6aa0a5433cb966606e699187300
MangoSalazar <l22440193@mochis.tecnm.mx>
2025-11-18 18:39:43 -0700

    Añadi las constantes a la clase registrar

--- Servidorsote/src/Protocolo.java
+++ Servidorsote/src/Protocolo.java
@@ -1,4 +1,22 @@
 
 public class Protocolo {
+    // Configuración del Servidor
+    public static final int PUERTO = 8080;
+    public static final int LIMITE_MENSAJES_GUEST = 3;
+
+    // Comandos de Gestión
+    public static final String CMD_LOGIN = "login";
+    public static final String CMD_REGISTER = "register";
+    public static final String CMD_REGISTRAR = "registrar"; 
+
+    // Prefijos de Mensajería
+    public static final String PREFIJO_PRIVADO = "@";
+    public static final String PREFIJO_GRUPAL = "%";
+    public static final String PREFIJO_BLOQUEO = "#";
     
+    // Validaciones
+    public static final int MIN_LONG_USER = 3;
+    public static final int MAX_LONG_USER = 15;
+    public static final int MIN_LONG_PASS = 3;
+    public static final int MAX_LONG_PASS = 10;
 }
